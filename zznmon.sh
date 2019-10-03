1e0cd9d0e13c940e37ec191e060bd7a52fb7b2b0
lizz <lizz@sensetime.com>
2019-10-03 18:58:55 +0800

    nmon
    Signed-off-by: lizz <lizz@sensetime.com>

---
+++ zznmon.sh
@@ -0,0 +1,13 @@
+ROOTDIR=/home/lizz/app
+
+cd $ROOTDIR
+wget http://sourceforge.net/projects/nmon/files/nmon16j.tar.gz -O nmon.tar.gz
+
+mkdir -p nmon/bin
+tar xf nmon.tar.gz -C nmon/bin
+
+cd $ROOTDIR/bin
+ln -s
+ln -s ../nmon/bin/nmon_x86_rhel75 nmon
+
+echo `pwd`
