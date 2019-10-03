7798ba2b3956e522c422f21615f3417309fff955
lizz <lizz@sensetime.com>
2019-10-03 17:53:09 +0800

    htop
    Signed-off-by: lizz <lizz@sensetime.com>

---
+++ zzhtop.sh
@@ -0,0 +1,13 @@
+ROOTDIR=/home/lizz/app
+
+cd $ROOTDIR
+wget http://hisham.hm/htop/releases/2.2.0/htop-2.2.0.tar.gz -O htop.tar.gz
+
+mkdir -p htop/src
+tar xf htop.tar.gz -C htop/src --strip-components 1
+
+cd htop/src
+
+./configure --prefix=$ROOTDIR/htop CFLAGS="-I$ROOTDIR/ncurses/include -I$ROOTDIR/ncurses/include/ncurses" LDFLAGS="-L$ROOTDIR/ncurses/lib" --disable-unicode
+
+make -j && make install
