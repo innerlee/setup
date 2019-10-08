f0e9622feb8849d10a1f70b3b174d441030da9ef
lizz <lizz@sensetime.com>
2019-10-08 13:49:23 +0800

    image magick
    Signed-off-by: lizz <lizz@sensetime.com>

---
+++ zzmagick.sh
@@ -0,0 +1,19 @@
+# install image magick
+
+ROOTDIR=/home/lizz/app
+mkdir -p $ROOTDIR
+cd $ROOTDIR
+
+wget https://imagemagick.org/download/ImageMagick.tar.gz -O magick.tar.gz
+
+mkdir -p magick/src
+tar xf magick.tar.gz -C magick/src --strip-components 1
+
+cd magick/src
+
+./configure --prefix=$ROOTDIR/magick
+
+make -j && make install
+
+cd ..
+echo magick installed on `pwd`
