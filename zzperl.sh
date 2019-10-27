ad85ecec913059665f629d58262165bba1fe049c
lizz <lizz@sensetime.com>
2019-10-27 21:35:32 +0800

    perl
    Signed-off-by: lizz <lizz@sensetime.com>

---
+++ zzperl.sh
@@ -0,0 +1,18 @@
+# install perl
+
+ROOTDIR=$HOME/app
+mkdir -p $ROOTDIR
+cd $ROOTDIR
+
+wget https://www.cpan.org/src/5.0/perl-5.30.0.tar.gz -O perl.tar.gz
+
+mkdir -p perl/src
+tar xf perl.tar.gz -C perl/src --strip-components 1
+
+cd perl/src
+./Configure -des -Dprefix=$ROOTDIR/perl
+# ./configure --prefix=$ROOTDIR/perl
+make -j && make install
+
+cd ..
+echo perl installed on $(pwd)
