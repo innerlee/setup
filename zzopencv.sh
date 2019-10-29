e1f6a496b9819de5db731831f379a94c9d7587fb
lizz <lizz@sensetime.com>
2019-10-29 23:00:32 +0800

    disable more
    Signed-off-by: lizz <lizz@sensetime.com>

--- zzopencv.sh
+++ zzopencv.sh
@@ -1,11 +1,11 @@
 # install opencv
 
-ROOTDIR=$HOME/app
+ROOTDIR=/mnt/lustrenvme/lizz/app
 mkdir -p $ROOTDIR
 cd $ROOTDIR
 
-wget https://github.com/opencv/opencv/archive/4.1.2.tar.gz -O opencv.tar.gz
-wget https://github.com/opencv/opencv_contrib/archive/4.1.2.tar.gz -O opencvcontrib.tar.gz
+cp /mnt/lustrenew/share/lizz/downloads/opencv-4.1.2.tar.gz opencv.tar.gz
+cp /mnt/lustrenew/share/lizz/downloads/opencv_contrib-4.1.2.tar.gz opencvcontrib.tar.gz
 
 mkdir -p opencv/src/opencv_contrib
 tar xf opencv.tar.gz -C opencv/src --strip-components 1
@@ -24,13 +24,32 @@ cmake \
     -DCMAKE_BUILD_TYPE=RELEASE \
     -DBUILD_opencv_cnn_3dobj=OFF \
     -DBUILD_opencv_dnn=OFF \
+    -DBUILD_opencv_datasets=OFF \
+    -DBUILD_opencv_aruco=OFF \
+    -DBUILD_opencv_tracking=OFF \
+    -DBUILD_opencv_text=OFF \
+    -DBUILD_opencv_stereo=OFF \
+    -DBUILD_opencv_saliency=OFF \
+    -DBUILD_opencv_rgbd=OFF \
+    -DBUILD_opencv_reg=OFF \
+    -DBUILD_opencv_ovis=OFF \
+    -DBUILD_opencv_matlab=OFF \
+    -DBUILD_opencv_freetype=OFF \
+    -DBUILD_opencv_dpm=OFF \
+    -DBUILD_opencv_face=OFF \
+    -DBUILD_opencv_dnn_superres=OFF \
+    -DBUILD_opencv_dnn_objdetect=OFF \
+    -DBUILD_opencv_bgsegm=OFF \
+    -DBUILD_opencv_cvv=OFF \
+    -DBUILD_opencv_ccalib=OFF \
+    -DBUILD_opencv_bioinspired=OFF \
     -DBUILD_opencv_dnn_modern=OFF \
     -DBUILD_opencv_dnns_easily_fooled=OFF \
     -DBUILD_JAVA=OFF \
     -DBUILD_opencv_python2=OFF \
     -DBUILD_NEW_PYTHON_SUPPORT=ON \
-    -DBUILD_opencv_python3=ON \
-    -DHAVE_opencv_python3=ON \
+    -DBUILD_opencv_python3=OFF \
+    -DHAVE_opencv_python3=OFF \
     -DPYTHON_DEFAULT_EXECUTABLE=$(which python) \
     -DWITH_OPENGL=ON \
     -DFORCE_VTK=OFF \
@@ -43,7 +62,7 @@ cmake \
     -DMKL_WITH_OPENMP=ON \
     -DWITH_XINE=ON \
     -DENABLE_PRECOMPILED_HEADERS=OFF \
-    -DCMAKE_INSTALL_PREFIX=/mnt/lustrenew/lizz/app/opencv \
+    -DCMAKE_INSTALL_PREFIX=/mnt/lustrenvme/lizz/app/opencv \
     -DPYTHON_EXECUTABLE="$HOME/anaconda3/bin/python" \
     -DPYTHON_LIBRARY="$HOME/anaconda3/lib/python3.7" \
     -DPYTHON3_LIBRARY="$HOME/anaconda3/lib/python3.7" \
