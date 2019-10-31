ba87fcf733de375a889a375188e746455f371e4c
lizz <lizz@sensetime.com>
2019-10-31 09:56:47 +0800

    better location
    Signed-off-by: lizz <lizz@sensetime.com>

--- zzffmpeg.sh
+++ zzffmpeg.sh
@@ -6,12 +6,13 @@ cd $ROOTDIR
 
 wget https://www.ffmpeg.org/releases/ffmpeg-4.2.1.tar.gz -O ffmpeg.tar.gz
 
-mkdir -p ffmpeg/src
-tar xf ffmpeg.tar.gz -C ffmpeg/src --strip-components 1
+mkdir -p src/ffmpeg
+tar xf ffmpeg.tar.gz -C src/ffmpeg --strip-components 1
 
-cd ffmpeg/src
-./configure --prefix=$ROOTDIR/ffmpeg
+cd src/ffmpeg
+./configure --prefix=$ROOTDIR
 make -j && make install
+
 
 cd ..
 echo ffmpeg installed on $(pwd)
