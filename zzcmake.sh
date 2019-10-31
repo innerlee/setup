807489e679fb6c1643e172e8170383c392f6eb50
lizz <lizz@sensetime.com>
2019-11-01 02:35:59 +0800

    cmake
    Signed-off-by: lizz <lizz@sensetime.com>

--- zzcmake.sh
+++ zzcmake.sh
@@ -6,12 +6,11 @@ cd $ROOTDIR
 
 wget https://github.com/Kitware/CMake/releases/download/v3.15.4/cmake-3.15.4.tar.gz -O cmake.tar.gz
 
-mkdir -p cmake/src
-tar xf cmake.tar.gz -C cmake/src --strip-components 1
+mkdir -p src/cmake
+tar xf cmake.tar.gz -C src/cmake --strip-components 1
 
-cd cmake/src
-./configure --prefix=$ROOTDIR/cmake
+cd src/cmake
+./configure --prefix=$ROOTDIR
 make -j && make install
 
-cd ..
-echo cmake installed on $(pwd)
+echo cmake installed on $ROOTDIR
