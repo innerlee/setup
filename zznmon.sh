486592575215441346f0a92ef4f0db437b4ba0b6
lizz <lizz@sensetime.com>
2019-11-02 23:25:40 +0800

    tiny
    Signed-off-by: lizz <lizz@sensetime.com>

--- zznmon.sh
+++ zznmon.sh
@@ -4,10 +4,10 @@ mkdir -p $ROOTDIR/bin
 cd $ROOTDIR
 wget http://sourceforge.net/projects/nmon/files/nmon16j.tar.gz -O nmon.tar.gz
 
-mkdir -p nmon/bin
-tar xf nmon.tar.gz -C nmon/bin
+mkdir -p src/nmon
+tar xf nmon.tar.gz -C src/nmon
 
 cd $ROOTDIR/bin
-ln -s ../nmon/bin/nmon_x86_rhel75 nmon
+ln -s ../src/nmon/nmon_x86_rhel75 nmon
 
 echo $(pwd)
