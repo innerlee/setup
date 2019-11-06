23ad135b77f61902051c94b2c88dd3eb76cd9a13
lizz <lizz@sensetime.com>
2019-11-06 18:11:47 +0800

    gmp
    Signed-off-by: lizz <lizz@sensetime.com>

---
+++ zzgmp.sh
@@ -0,0 +1,32 @@
+# install cmake
+set -e
+
+ROOTDIR=${ZZROOT:-$HOME/app}
+NAME="gmp"
+TYPE=".tar.xz"
+FILE="$NAME$TYPE"
+DOWNLOADURL="https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz"
+echo $NAME will be installed in $ROOTDIR
+
+mkdir -p $ROOTDIR/downloads
+cd $ROOTDIR
+
+if [ -f "downloads/$FILE" ]; then
+    echo "downloads/$FILE exist"
+else
+    echo "$FILE does not exist, downloading..."
+    wget $DOWNLOADURL -O $FILE
+    mv $FILE downloads/
+fi
+
+mkdir -p src/$NAME
+tar xf downloads/$FILE -C src/$NAME --strip-components 1
+
+cd src/$NAME
+
+./configure --prefix=$ROOTDIR
+make -j && make install
+
+make check
+
+echo $NAME installed on $ROOTDIR
