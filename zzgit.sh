b5415eb0bad6c70a37b4dacbabbef7e2f10b5d52
lizz <lizz@sensetime.com>
2019-11-07 20:12:16 +0800

    git
    Signed-off-by: lizz <lizz@sensetime.com>

--- zzgit.sh
+++ zzgit.sh
@@ -1,22 +1,42 @@
 # install git
+set -e
+
+ROOTDIR=${ZZROOT:-$HOME/app}
+NAME="git"
+TYPE=".tar.gz"
+FILE="$NAME$TYPE"
+DOWNLOADURL="https://codeload.github.com/git/git/tar.gz/v2.23.0"
+echo $NAME will be installed in $ROOTDIR
+echo "hey, install libz and libcurl first"
+
+mkdir -p $ROOTDIR/downloads
+cd $ROOTDIR
+
+if [ -f "downloads/$FILE" ]; then
+    echo "downloads/$FILE exist"
+else
+    echo "$FILE does not exist, downloading..."
+    wget $DOWNLOADURL -O $FILE
+    mv $FILE downloads/
+fi
+
+mkdir -p src/$NAME
+tar xf downloads/$FILE -C src/$NAME --strip-components 1
+
+cd src/$NAME
+
+make configure
+./configure --prefix=$ROOTDIR
+make -j && make install
+
+echo $NAME installed on $ROOTDIR
+
+# install git
 ROOTDIR=${ZZROOT:-$HOME/app}
 mkdir -p $ROOTDIR
 cd $ROOTDIR
 
-echo "hey, install libz and libcurl first"
 
-wget https://codeload.github.com/git/git/tar.gz/v2.23.0 -O git.tar.gz
-
-mkdir -p src/git
-tar xf git.tar.gz -C src/git --strip-components 1
-
-cd src/git
-
-make configure
 
 # ./configure --prefix=$ROOTDIR CFLAGS="-I$ROOTDIR/libcurl/include -I$ROOTDIR/libcurl/include/curl" LDFLAGS="-L$ROOTDIR/libcurl/lib"
 # ./configure --prefix=$ROOTDIR CFLAGS="-I$ROOTDIR/libz/include -I$ROOTDIR/libcurl/include -I$ROOTDIR/libcurl/include/curl" LDFLAGS="-L$ROOTDIR/libz/lib -L$ROOTDIR/libcurl/lib"
-./configure --prefix=$ROOTDIR
-make -j && make install
-
-echo git installed on $ROOTDIR
