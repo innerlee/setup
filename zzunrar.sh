32a21a91b2c98610718fb6d75871dd69ac2971ec
lizz <lizz@sensetime.com>
2019-11-08 14:26:46 +0800

    unrar
    Signed-off-by: lizz <lizz@sensetime.com>

---
+++ zzunrar.sh
@@ -0,0 +1,30 @@
+# install unrar
+set -e
+
+ROOTDIR=${ZZROOT:-$HOME/app}
+NAME="unrar"
+TYPE=".tar.gz"
+FILE="$NAME$TYPE"
+DOWNLOADURL="http://www.rarlab.com/rar/unrarsrc-5.8.3.tar.gz"
+echo $NAME will be installed in $ROOTDIR
+
+mkdir -p $ROOTDIR/downloads
+cd $ROOTDIR
+
+if [ -f "downloads/$FILE" ]; then
+    echo "downloads/$FILE exist"
+else
+    echo "$FILE does not exist, downloading..."
+    wget $DOWNLOADURL -O $FILE
+    mv $FILE downloads/
+fi
+
+mkdir -p src/$NAME
+tar xf downloads/$FILE -C src/$NAME --strip-components 1
+
+cd src/$NAME
+
+make -f makefile
+install -v -m755 unrar $ROOTDIR/bin
+
+echo $NAME installed on $ROOTDIR
