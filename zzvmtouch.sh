1574d387325b3ae1b31cf163d2a7c753c529a9d2
lizz <lizz@sensetime.com>
2019-11-09 11:11:51 +0800

    vmtouch
    Signed-off-by: lizz <lizz@sensetime.com>

---
+++ zzvmtouch.sh
@@ -0,0 +1,30 @@
+# install vmtouch
+set -e
+
+ROOTDIR=${ZZROOT:-$HOME/app}
+NAME="vmtouch"
+TYPE=".tar.gz"
+FILE="$NAME$TYPE"
+DOWNLOADURL="https://github.com/hoytech/vmtouch/archive/v1.3.1.tar.gz"
+echo $NAME will be installed in $ROOTDIR
+
+mkdir -p $ROOTDIR/downloads
+cd $ROOTDIR
+
+if [ -f "downloads/$FILE" ]; then
+    echo "downloads/$FILE exist"
+else
+    echo "$FILE does not exist, downloading..."
+    wget $DOWNLOADURL -O $FILE
+    mv $FILE downloads/
+fi
+
+mkdir -p src/$NAME
+tar xf downloads/$FILE -C src/$NAME --strip-components 1
+
+cd src/$NAME
+
+export PREFIX=$ROOTDIR
+make -j && make install
+
+echo $NAME installed on $ROOTDIR
