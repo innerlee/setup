6d1a3693986cf9d814180f443a36e714214dc5e9
lizz <lizz@sensetime.com>
2019-12-24 22:12:56 +0800

    libnl
    Signed-off-by: lizz <lizz@sensetime.com>

---
+++ zzlibnl.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# install libnl
+set -e
+
+ROOTDIR=${ZZROOT:-$HOME/app}
+NAME="libnl"
+TYPE=".tar.gz"
+FILE="$NAME$TYPE"
+DOWNLOADURL="https://github.com/thom311/libnl/releases/download/libnl3_5_0/libnl-3.5.0.tar.gz"
+echo $NAME will be installed in $ROOTDIR
+
+mkdir -p $ROOTDIR/downloads
+cd $ROOTDIR
+
+if [ -f "downloads/$FILE" ]; then
+    echo "downloads/$FILE exist"
+else
+    echo "$FILE does not exist, downloading from $DOWNLOADURL"
+    wget $DOWNLOADURL -O $FILE
+    mv $FILE downloads/
+fi
+
+mkdir -p src/$NAME
+tar xf downloads/$FILE -C src/$NAME --strip-components 1
+
+cd src/$NAME
+
+./configure --prefix=$ROOTDIR
+make -j && make install
+
+echo $NAME installed on $ROOTDIR
