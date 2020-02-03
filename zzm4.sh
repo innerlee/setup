438d25797717a24759ba5ac633d39f144220812b
lizz <lizz@sensetime.com>
2020-02-03 23:11:05 +0800

    m4

---
+++ zzm4.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# install m4
+set -e
+
+ROOTDIR=${ZZROOT:-$HOME/app}
+NAME="m4"
+TYPE=".tar.gz"
+FILE="$NAME$TYPE"
+DOWNLOADURL="http://ftp.gnu.org/gnu/m4/m4-latest.tar.gz"
+echo $NAME will be installed in $ROOTDIR
+
+mkdir -p $ROOTDIR/downloads
+cd $ROOTDIR
+
+if [ -f "downloads/$FILE" ]; then
+    echo "downloads/$FILE exist"
+else
+    echo "$FILE does not exist, downloading from $DOWNLOADURL"
+    wget $DOWNLOADURL -O $FILE
+    mv $FILE downloads/
+fi
+
+mkdir -p src/$NAME
+tar xf downloads/$FILE -C src/$NAME --strip-components 1
+
+cd src/$NAME
+
+./configure --prefix=$ROOTDIR
+make -j$(nproc) && make install
+
+echo $NAME installed on $ROOTDIR
