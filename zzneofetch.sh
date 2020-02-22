9f09c058140ad23b9bd219c07d0ea2ba5a31bd3f
lizz <lizz@sensetime.com>
2020-02-22 18:03:38 +0800

    neofetch
    Signed-off-by: lizz <lizz@sensetime.com>

---
+++ zzneofetch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# install neofetch
+set -e
+
+ROOTDIR=${ZZROOT:-$HOME/app}
+NAME="neofetch"
+TYPE=".tar.gz"
+FILE="$NAME$TYPE"
+DOWNLOADURL="https://codeload.github.com/dylanaraps/neofetch/tar.gz/master"
+echo $NAME will be installed in "$ROOTDIR"
+
+mkdir -p "$ROOTDIR/downloads"
+cd "$ROOTDIR"
+
+if [ -f "downloads/$FILE" ]; then
+    echo "downloads/$FILE exist"
+else
+    echo "$FILE does not exist, downloading from $DOWNLOADURL"
+    wget $DOWNLOADURL -O $FILE
+    mv $FILE downloads/
+fi
+
+mkdir -p src/$NAME
+tar xf downloads/$FILE -C src/$NAME --strip-components 1
+
+cd src/$NAME
+
+cp neofetch $ROOTDIR/bin
+
+echo $NAME installed on "$ROOTDIR"
