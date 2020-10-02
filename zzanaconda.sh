e4e97d585ca6d1320dc6e144d1d42d6ee7dcf485
lizz <363664470@qq.com>
2020-10-02 14:05:46 +0800

    conda

--- zzanaconda.sh
+++ zzanaconda.sh
@@ -2,6 +2,6 @@
 
 cd || exit
 
-wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
 
 sh Anaconda3-2020.02-Linux-x86_64.sh
