b6b773a10beaf0b3997783eb459edee30cf2ce12
段佳琦 <SENSETIME\duanjiaqi@cn0014004614l.domain.sensetime.com>
2021-03-11 20:55:04 +0800

    ffmpeg-10bit>>> 2. install

--- zzffmpeg-10bit.sh
+++ zzffmpeg-10bit.sh
@@ -21,7 +21,299 @@ else
 fi
 
 mkdir -p src/$NAME
-tar xf downloads/$FILE -C src/$NAME --strip-components 1
-
+tar xf downloads/$FILE -C src/$NAME
 cd src/$NAME
+wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+wget https://www.zlib.net/fossils/zlib-1.2.9.tar.gz
+wget https://mirrors.tuna.tsinghua.edu.cn/apache//ant/binaries/apache-ant-1.9.15-bin.tar.gz
+ls *.tar* | xargs -P 2 -i tar xf {}
+
+export CFLAGS="-I$ROOTDIR/include"
+export CPPFLAGS="-I$ROOTDIR/include"
+export LDFLAGS="-L$ROOTDIR/lib -L$ROOTDIR/lib64"
+export PKG_CONFIG_PATH=$ROOTDIR/lib/pkgconfig:$ROOTDIR/lib64/pkgconfig:$ROOTDIR/share/pkgconfig:$PKG_CONFIG_PATH
+export PATH=$ROOTDIR/bin:$ROOTDIR/src/ffmpeg-10bit/apache-ant-1.9.15/bin/:$PATH
+
+# install bzip
+echo -e "\033[32m[-23] >>> install bzip... \033[0m"
+cd bzip2-1.0.6/
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -f Makefile-libbz2_so > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install PREFIX=$ROOTDIR > log_make_install.txt 2>&1
+
+# install xz
+echo -e "\033[32m[-22]>>> install xz... \033[0m"
+cd ../xz-5.2.2
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install libxml
+echo -e "\033[32m[-21]>>> install libxml... \033[0m"
+cd ../libxml2-2.9.3
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR --without-python > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install libogg
+echo -e "\033[32m[-20]>>> install libogg... \033[0m"
+cd ../libogg-1.3.2
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install zimg
+echo -e "\033[32m[-19]>>> install zimg... \033[0m"
+cd ../zimg-master
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+bash autogen.sh
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install zlib
+echo -e "\033[32m[-18]>>> install zlib... \033[0m"
+cd ../zlib-1.2.9
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install harfbuzz
+echo -e "\033[32m[-17]>>> install harfbuzz... \033[0m"
+cd ../harfbuzz-1.2.3
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install fribidi
+echo -e "\033[32m[-16]>>> install fribidi... \033[0m"
+cd ../fribidi-0.19.7
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install libbluray
+# note: need ant、java
+echo -e "\033[32m[-15]>>> install libbluray... \033[0m"
+cd ../libbluray-0.9.2
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install gmp
+echo -e "\033[32m[-14]>>> install gmp... \033[0m"
+cd ../gmp-6.1.0
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+echo "      >>> make check...    [log > "$SHELL_FOLDER/log_make_check.txt ]
+make check > log_make_check.txt 2>&1
+
+# install nettle
+echo -e "\033[32m[-13]>>> install nettle... \033[0m"
+cd ../nettle-2.7.1
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install gnutls
+echo -e "\033[32m[-12]>>> install gnutls... \033[0m"
+cd ../gnutls-3.3.22
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install opus
+echo -e "\033[32m[-11]>>> install opus... \033[0m"
+cd ../opus-1.1.2
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install libvorbis
+echo -e "\033[32m[-10]>>> install libvorbis... \033[0m"
+cd ../libvorbis-1.3.5
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install yasm
+echo -e "\033[32m[-9]>>> install yasm... \033[0m"
+cd ../yasm-1.3.0
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install libvpx
+echo -e "\033[32m[-8]>>> install libvpx... \033[0m"
+cd ../libvpx-master
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install libass
+echo -e "\033[32m[-7]>>> install libass... \033[0m"
+cd ../libass-0.13.2
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install libiconv
+echo -e "\033[32m[-6]>>> install libiconv... \033[0m"
+cd ../libiconv-1.14
+sed -i '698i\\/\*' srclib/stdio.in.h
+sed -i '699a\\*\/' srclib/stdio.in.h
+sed -i '700a\\#if\ defined\(__GLIBC__\)\ \&\&\ \!defined\(__UCLIBC__\)\ \&\&\ \!__GLIBC_PREREQ\(2,\ 16\)' srclib/stdio.in.h
+sed -i '701a\\ _GL_WARN_ON_USE\ \(gets,\ \"gets\ is\ a\ security\ hole\ \-\ use\ fgets\ instead\"\)\;' srclib/stdio.in.h
+sed -i '702a\\#endif' srclib/stdio.in.h
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install lame
+echo -e "\033[32m[-5]>>> install lame... \033[0m"
+cd ../lame-3.99.5
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install freetype
+echo -e "\033[32m[-4]>>> install freetype... \033[0m"
+cd ../freetype-2.6.2
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install fontconfig
+echo -e "\033[32m[-3]>>> install fontconfig... \033[0m"
+cd ../fontconfig-2.11.1
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install x264
+echo -e "\033[32m[-2]>>> install x264... \033[0m"
+cd ../x264-snapshot-20160319-2245-stable
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure --prefix=$ROOTDIR --enable-shared --enable-static --bit-depth=10 > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+# install x265
+echo -e "\033[32m[-1]>>> install x265... \033[0m"
+cd ../x264-snapshot-20160319-2245-stable/build
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> camke...         [log > "$SHELL_FOLDER/log_camke.txt ]
+camke ../source/ > log_camke.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install DESTDIR=$ROOTDIR > log_make_install.txt 2>&1
+mv $ROOTDIR/usr/local/lib/libx265.a $ROOTDIR/lib/
+mv $ROOTDIR/usr/local/lib/libx265.so $ROOTDIR/lib/
+mv $ROOTDIR/usr/local/lib/libx265.so.79 $ROOTDIR/lib/
+sed -i '1d' $ROOTDIR/usr/local/lib/pkgconfig/x265.pc
+echo prefix=$ROOTDIR > tmptmp
+cat tmptmp $ROOTDIR/usr/local/lib/pkgconfig/x265.pc > $ROOTDIR/lib/pkgconfig/x265.pc
+rm $ROOTDIR/usr/local/lib/pkgconfig/x265.pc
+rm tmptmp
+mv $ROOTDIR/usr/local/bin/x265 $ROOTDIR/bin/
+mv $ROOTDIR/usr/local/include/* $ROOTDIR/include/
+
+# install ffmpeg
+echo -e "\033[32m[0]>>> install ffmpeg... \033[0m"
+cd ../../ffmpeg
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+echo "      >>> configure...     [log > "$SHELL_FOLDER/log_configure.txt ]
+./configure  --prefix=$ROOTDIR --disable-doc --disable-debug --enable-avisynth --enable-dxva2 --enable-bzlib --enable-fontconfig --enable-gmp --enable-gnutls --enable-iconv --enable-libass --enable-libbluray --enable-libfreetype --enable-libfribidi --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265 --enable-libzimg --enable-lzma --enable-zlib --enable-gpl --enable-version3 --enable-nonfree --arch=x86_64  > log_configure.txt 2>&1
+echo "      >>> make...          [log > "$SHELL_FOLDER/log_make.txt ]
+make -j 4 > log_make.txt 2>&1
+echo "      >>> make install...  [log > "$SHELL_FOLDER/log_make_install.txt ]
+make install > log_make_install.txt 2>&1
+
+echo "======================================================"
 echo $NAME installed on "$ROOTDIR"
